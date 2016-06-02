0433d410eae405318024d28f4014f3a78db480d9
Nicholas Davidson <ndavidson.sf@me.com>
2016-06-01 21:08:14 -0700

    adds psuedo code

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,12 @@
+=begin
+psuedo code: 
+encryption
+- determine length of string
+- for each letter in the string, advance one letter
+- display new letters
+decryption
+- determine length of string
+- for each letter in the string, go back one letter in the alphabet
+- display new letters
+
+=end
