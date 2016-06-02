40a01ddd99d2500d5ab065afd3382480430ff5fb
Nicholas Davidson <ndavidson.sf@me.com>
2016-06-01 21:51:39 -0700

    adds encrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -6,7 +6,22 @@ encryption
 - display new letters
 decryption
 - determine length of string
-- for each letter in the string, go back one letter in the alphabet
+- for each letter in the string, go back one letter in the alphabet using the reverse index
 - display new letters
 
 =end
+
+
+
+
+def encrypt(string)
+	index = 0
+	while index < string.length
+		string[index] = string[index].next!
+		puts string[index]
+		index += 1
+	end
+end
+
+encrypt('abc')
+
