31e988196f795e255f6879be252e3a6f95802779
Nicholas Davidson <ndavidson.sf@me.com>
2016-06-02 22:02:08 -0700

    adds interface

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -8,10 +8,16 @@ decryption
 - determine length of string
 - for each letter in the string, go back one letter in the alphabet using the reverse index
 - display new letters
+interface
+- create menu (1. Encrypt, 2. Decrypt 3. exit)
+- use a while loop to loop program until user exits
+- gather user input
+- execute appropriate method
 
 =end
 
 
+# Method Declarations
 
 # encryption method
 def encrypt(string)
@@ -35,7 +41,12 @@ def encrypt(string)
 		end
 		index += 1
 	end
-	puts message
+	puts
+	puts "-----------------------------------------"
+	puts "              -RESULTS-"
+	puts
+	puts "Your encrypted password is: #{message}."
+	puts "-----------------------------------------"
 end
 
 # decryption method
@@ -60,12 +71,44 @@ def decrypt(string)
 
 		index += 1
 	end
-	puts message
+	puts
+	puts "-----------------------------------------"
+	puts "              -RESULTS-"
+	puts
+	puts "Your decrypted password is: #{message}." 
+	puts "-----------------------------------------"
 end
 
 
 
+# Driver Code
 
+puts "-----------------------------------------------"
+puts "            The Encyrption Program"
+puts "-----------------------------------------------"
+puts "                  -Main Menu-"
+puts "Please select: "
+puts "      1. Encyrption"
+puts "      2. Decryption"
+useroption = gets.chomp
+	if useroption == "1"
+		print "Enter password to encrypt: "
+		string = gets.chomp
+		encrypt(string)
+	elsif useroption == "2"
+		print "Enter password to decrypt: "
+		string = gets.chomp
+		decrypt(string)
+	else
+		exit
+	end
+
+
+
+
+
+
+=begin
 encrypt('abc ') # this one includes a space for debugging purposes
 encrypt('zed')
 decrypt("bcd")
@@ -73,5 +116,5 @@ decrypt("afe")
 
 decrypt(encrypt("swordfish")) # this doesn't work and I am not sure why so I can't add a comment explaining why it works
 
-
+=end
 
