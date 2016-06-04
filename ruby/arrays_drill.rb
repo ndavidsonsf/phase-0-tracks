f33352e068eea93f313f795ad1b88fc37daed427
Nicholas Davidson <ndavidson.sf@me.com>
2016-06-03 18:10:10 -0700

    initial commit

---
+++ ruby/arrays_drill.rb
@@ -0,0 +1,8 @@
+
+# Initialize an empty array and store it in a variable (you can choose the name). Print the variable using p.
+new_array = []
+p new_array
+
+# Add five items to your array. Print the array.
+new_array.push("Italian Roast", "Gold Coast", "Veranda Blend", "Pike Place Roast")
+p new_array
