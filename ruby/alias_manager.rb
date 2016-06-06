7d4390376680e36ce8adac9ae77830d1f453b18a
Nicholas Davidson <ndavidson.sf@me.com>
2016-06-05 17:52:13 -0700

    inital commit and pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,17 @@
+=begin
+Pseudocode
+
+- Asks User for name
+- break the input into an array of two names
+	- reverse the order on the names array
+- break the names into an array of letters
+	- use conditionals to:
+		-shift the vowels to the next in order
+		-shift the consonants to the next in the alphabet
+	- rejoin the the letters
+	- rejoin the strings
+	- print for the user
+
+=end
+
+
