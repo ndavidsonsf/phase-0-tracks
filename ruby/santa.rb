977e8a0711b714e7bbcce9eb45150c54bbf44b49
Nicholas Davidson <ndavidson.sf@me.com>
2016-06-11 09:58:13 -0700

    adds Release0/ Santa class and instance methods

---
+++ ruby/santa.rb
@@ -0,0 +1,21 @@
+class Santa
+
+	def speak
+		puts "Ho, ho, ho!  Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie} cookie!"
+	end
+
+	def initialize
+		puts "Initializing Santa instance..."
+	end
+end
+
+#DRIVER CODE
+
+saint_nick = Santa.new
+saint_nick.speak
+saint_nick.eat_milk_and_cookies('chocolate chip')
+
