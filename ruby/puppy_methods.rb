efd9cf469cff5921d9f31755c6808027ec97f347
Nicholas Davidson <ndavidson.sf@me.com>
2016-06-10 22:51:37 -0700

    adds Class Behaviors

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,33 @@ class Puppy
     toy
   end
 
+  def speak(quantity)
+  	puts "Woof! " * quantity
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(years)
+  	years = years * 7
+  	puts "I am #{years} in dog years."
+  end
+
+  def shakes
+  	puts "*raises paw*"
+  end
+
+
 end
+
+# Driver Code
+
+fido = Puppy.new
+
+fido.fetch('ball')
+fido.speak(3)
+fido.roll_over
+fido.dog_years(3)
+fido.shakes
 
