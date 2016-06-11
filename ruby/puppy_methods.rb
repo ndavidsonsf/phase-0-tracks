0b88f83865012f4abd8b45c0021f81d60574228c
Nicholas Davidson <ndavidson.sf@me.com>
2016-06-10 23:19:28 -0700

    adds Cyclist class and instance methods/creates instances and iterates through all instances in a data structure

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -22,16 +22,54 @@ class Puppy
   	puts "*raises paw*"
   end
 
+  def initialize
+  	puts "Initializing new puppy instance..."
+  end
+
 
 end
 
 # Driver Code
 
+# Creates new instance of Puppy class
+# calls all Puppy class instance methods on fido
 fido = Puppy.new
-
 fido.fetch('ball')
 fido.speak(3)
 fido.roll_over
 fido.dog_years(3)
 fido.shakes
+
+# Creates Cyclist class and instance methods
+class Cyclist
+
+	def pedal(intensity)
+		puts "I am pedaling #{intensity}!"
+	end
+
+	def brake(intensity)
+		puts "I am braking #{intensity}!"
+	end
+
+	def initialize
+		puts "I am getting on my bike..."
+	end
+
+end
+
+# loop to create 50 instances of Cyclist class
+# adds those instances to an array
+
+count = 0
+cyclists = []
+until count == 50
+	cyclists.push(Cyclist.new)
+	count += 1
+end
+
+# interates through the array and calls each instance method
+cyclists.each do |rider|
+	rider.pedal('fast')
+	rider.brake('hard')
+end
 
