d815a2e241367a8f906021ce60982e285cc3a4d8
Nicholas Davidson <ndavidson.sf@me.com>
2016-06-12 16:30:43 -0700

    adds code for test conditions

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,32 @@
+class TodoList
+
+	def initialize(list)
+		@list = list
+	end
+
+	def get_items
+		@list
+	end
+
+	def add_item(item)
+		@list << item
+		@list
+	end
+
+	def delete_item(item)
+		@list.delete(item)
+		@list
+	end
+
+	def get_item(index)
+		@list[index]
+	end
+
+	
+
+
+
+end
+
+
+#TodoList.new("do the dishes", "mow the lawn")
