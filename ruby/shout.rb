19dac61725734749fc01af184a64809181b73a3f
Nicholas Davidson <ndavidson.sf@me.com>
2016-06-12 11:05:27 -0700

    adds module methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,5 +1,12 @@
 # Release 1 - Write a Simple Module
 
 module Shout
+	def self.yell_angrily(words)
+		words + "!!!" + " :("
+	end
+
+	def self.yelling_happily(more_words)
+		more_words + "!!!" + " :)"
+	end
 
 end
