f66f6c99fd6ff59a5125f9355363c64f47e1e4e3
Nicholas Davidson <ndavidson.sf@me.com>
2016-06-12 11:29:05 -0700

    adds Module5 Release2

---
+++ ruby/flight.rb
@@ -0,0 +1,25 @@
+# Release 2 - Mix in Module
+
+module Flight
+
+	def take_off(altitude)
+		puts "Taking off and ascending until reaching #{altitude} ..."
+	end
+end
+
+
+class Bird
+	include Flight
+end
+
+
+class Plane
+	include Flight
+end
+
+# Test code
+bird = Bird.new
+bird.take_off(800)
+
+plane = Plane.new
+plane.take_off(30000)
