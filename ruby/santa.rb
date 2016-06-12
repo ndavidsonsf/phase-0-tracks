3442c179385d9bc93e327aa309faa89723cd213d
Nicholas Davidson <ndavidson.sf@me.com>
2016-06-11 17:33:07 -0700

    adds Release4

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,8 @@
 class Santa
 
+	attr_reader = :ethnicity
+	attr_accessor = :gender, :age
+
 	def speak
 		puts "Ho, ho, ho!  Haaaappy holidays!"
 	end
@@ -8,8 +11,20 @@ class Santa
 		puts "That was a good #{cookie} cookie!"
 	end
 
+	def celebrate_birthday
+		@age += 1
+		
+	end
+
+
+	def get_mad(reindeer_name)
+		mad_at = @reindeer_ranking.delete(reindeer_name)
+		@reindeer_ranking << reindeer_name
+		p @reindeer_ranking
+	end
+
 	def initialize(gender, ethnicity, age)
-		puts "Initializing Santa instance..."
+		# puts "Initializing Santa instance..."
 		@gender = gender
 		@ethnicity = ethnicity
 		@age = age
@@ -26,35 +41,9 @@ class Santa
 		]
 	end
 
-	def celebrate_birthday
-		@age += 1
-		
+	def print
+		puts puts "Recorded: Santa identifying as #{@gender} and #{@ethnicity} of age #{@age}."
 	end
-
-	def get_mad_at(reindeer_name)
-		mad_at = @reindeer_ranking.delete(reindeer_name)
-		@reindeer_ranking << reindeer_name
-		p @reindeer_ranking
-	end
-
-	# setter methods of attributes
-	def gender=(new_gender)
-		@gender = new_gender
-	end
-
-	# getter methods of attributes
-	def age
-		@age
-	end
-
-	def ethnicity
-		@ethnicity
-	end
-
-	def gender
-		@gender
-	end
-
 
 end
 
@@ -91,19 +80,36 @@ example_ethnicities = [
 
 
 # testing release1
-=begin
+
 santas = []
 
 santas << Santa.new(example_genders.sample, example_ethnicities.sample, rand(100))
 santas << Santa.new(example_genders.sample, example_ethnicities.sample, rand(100))
 santas << Santa.new(example_genders.sample, example_ethnicities.sample, rand(100))
-=end
+
 
 # testing release 2
 
+=begin
 saint_nick = Santa.new(example_genders.sample, example_ethnicities.sample, rand(100))
 puts "Recorded: Santa identifying as #{saint_nick.gender} and #{saint_nick.ethnicity} of age #{saint_nick.age}."
 kris_kringle = Santa.new('male', 'white', 70)
 puts "Recorded: Santa identifying as #{kris_kringle.gender} and #{kris_kringle.ethnicity} of age #{kris_kringle.age}."
 puts "Santa just celebrated a birthday, they are now #{kris_kringle.celebrate_birthday} years old."
-saint_nick.get_mad_at("Rudolph")
+saint_nick.get_mad("Rudolph")
+saint_nick.gender = 'questioning'
+puts "Recorded: Santa identifying as #{saint_nick.gender} and #{saint_nick.ethnicity} of age #{saint_nick.age}."
+=end
+
+count = 0
+until count == 10
+	santas.push(Santa.new(example_genders.sample, example_ethnicities.sample, rand(140)))
+	count += 1
+end
+
+santas.each do |num|
+	num.print
+end
+
+
+
