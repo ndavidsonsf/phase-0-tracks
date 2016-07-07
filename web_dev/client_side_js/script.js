f391e7e9bef3249e7eeec0c25fdec1b80e757d96
Nicholas Davidson <ndavidson.sf@me.com>
2016-07-07 15:46:16 -0700

    adds EventLister into new DOM elements

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -5,3 +5,15 @@ var h2s = document.querySelectorAll('h2'), i = h2s.length;
 while (i --) {
 	h2s[i].style.color = "white";
 }
+
+function ohYouDidNot() {
+	var el = document.createElement('div');
+	el.innerHTML = '<img id = "tinaphoto" src = "ohyoudidnot.gif">';
+	document.body.appendChild(el);
+
+}
+
+//var photo = document.getElementById("footer");
+//photo.addEventListener("click", ohYouDidNot);
+
+document.getElementById('footer').addEventListener('click', ohYouDidNot);
